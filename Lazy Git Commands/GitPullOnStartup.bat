dddca5c0061845a98082d840c2d87b1b2bdfba49
NotPieman20 <simonkeogh1249@gmail.com>
2019-02-27 20:08:43 +0000

    Lazy git commands added

---
+++ Lazy Git Commands/GitPullOnStartup.bat
@@ -0,0 +1,6 @@
+cd C:\\Place\\your\\git_directory\\here
+git pull 
+
+
+cd C:\\Place\\your\\git_directory\\here
+git pull 
---
+++ Lazy Git Commands/LazyPushToGit.bat
@@ -0,0 +1,11 @@
+cd C:\\Place\\your\\git_directory\\here
+git pull
+git add .
+git commit -m "This was a lazy commit"
+git push
+
+cd C:\\Place\\your\\git_directory\\here
+git pull 
+git add .
+git commit -m "This was a lazy commit"
+git push
